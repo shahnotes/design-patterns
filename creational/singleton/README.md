53de125b37378075d18c940273a800566fb02186
Harsh Shah <harsh.shah@siimteq.com>
2025-02-25 23:00:56 +0530

    Singleton

---
+++ creational/singleton/README.md
@@ -0,0 +1,4 @@
+# Singleton Pattern - Anti Pattern
+
+The Singleton is a creational pattern that ensures a class has at most one instance and provides a global point of access to this instance.
+
---
+++ creational/singleton/Singleton.php
@@ -0,0 +1,35 @@
+<?php
+
+class Singleton
+{
+    private static $instance;
+
+    private function __construct()
+    {
+        $this->init();
+    }
+
+    public function __clone()
+    {
+
+    }
+
+    public static function getInstance()
+    {
+        if ( ! isset(self::$instance)) {
+            self::$instance = new Singleton();
+        }
+
+        return self::$instance;
+    }
+
+    private function init()
+    {
+        echo "Singleton init" . PHP_EOL;
+    }
+}
+
+$instance = Singleton::getInstance();
+$instance2 = Singleton::getInstance();
+
+print_r($instance === $instance2);
